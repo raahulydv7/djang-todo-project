4756f46dda73e4a2c02b43711dcc6f8e048692cf
raahulydv7 <rahul7879yadav@gmail.com>
2025-08-23 17:34:57 +0530

    admin: category model

--- todo/models.py
+++ todo/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+from django.conf import settings
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+    
